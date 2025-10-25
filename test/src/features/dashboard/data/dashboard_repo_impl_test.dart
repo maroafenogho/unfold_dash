461848379b1a474951375ab9817c77facee27876
Oghenemaro Afenogho <maroafenogho@gmail.com>
2025-10-25 03:55:04 +0100

    Added tests to repository

--- test/src/features/dashboard/data/dashboard_repo_impl_test.dart
+++ test/src/features/dashboard/data/dashboard_repo_impl_test.dart
@@ -22,4 +22,37 @@ void main() {
     expect(data.right.first.mood, 2);
     expect(data.right.first.note, 'Bad sleep');
   });
+
+  group('Check for large data points', () {
+    test('get 10000 datapoints', () async {
+      final data = await repo.generateLargeBioData(10000);
+
+      expect(data.isRight, true);
+      expect(data.right.length, 10000);
+    });
+
+    test('get 10000 datapoints and decimate', () async {
+      final data = await repo.generateLargeBioData(10000);
+
+      expect(data.isRight, true);
+      expect(data.right.length, 10000);
+
+      final decimatedvalues = repo.decimateData(data.right, 500);
+
+      expect(decimatedvalues.isRight, true);
+      expect(decimatedvalues.right.length <= 1500, true);
+    });
+
+    test('get 4 datapoints and try to decimate', () async {
+      final data = await repo.generateLargeBioData(4);
+
+      expect(data.isRight, true);
+      expect(data.right.length, 4);
+
+      final decimatedvalues = repo.decimateData(data.right, 5);
+
+      expect(decimatedvalues.isRight, true);
+      expect(decimatedvalues.right.length, 4);
+    });
+  });
 }
